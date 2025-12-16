fe61ace49a1391e5db2a2154e019ab0965c8df5b
GreatFi <greatemmanuel923@gmail.com>
2025-12-17 00:23:20 +0100

    Move SECRET_KEY to environment variable

--- secureHome/settings.py
+++ secureHome/settings.py
@@ -12,8 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 from decouple import config
 from django.core.mail import send_mail
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-075(c-0bl)-!2(6u=vw&*o=kzwxn@58h@0iq$ga)qc-32^-v92'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

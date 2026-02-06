81ab6cbc741a3fede8fa3ee58cf8665a92fd0182
GreatFi <greatemmanuel923@gmail.com>
2026-02-06 22:43:01 +0100

    Prod fixes

--- secureHome/settings.py
+++ secureHome/settings.py
@@ -32,8 +32,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
 ALLOWED_HOSTS = ['.railway.app', 'localhost', '127.0.0.1']
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')

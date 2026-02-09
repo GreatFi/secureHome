c4e5ee9fbd1f46f438acea12658d49543aff709e
GreatFi <greatemmanuel923@gmail.com>
2026-02-09 12:42:48 +0100

    Project level static folder removed

--- secureHome/settings.py
+++ secureHome/settings.py
@@ -158,7 +158,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 LOGIN_REDIRECT_URL = 'homepage/'

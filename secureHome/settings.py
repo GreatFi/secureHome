ad59671adda9fd5e415d049434fc78b4ff8ceaa7
GreatFi <greatemmanuel923@gmail.com>
2026-03-15 23:35:44 +0100

    cloudinary removal

--- secureHome/settings.py
+++ secureHome/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
+    # 'cloudinary_storage',
     'django.contrib.staticfiles',
     'Secur',
     'channels',
